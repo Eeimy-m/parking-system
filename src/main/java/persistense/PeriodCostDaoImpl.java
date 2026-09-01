8b42ce706bd195d636dced186c5b8c3ddd807d4e
Elisa <eeimymiura@gmail.com>
2026-09-01 14:56:00 -0300

    feat: PeriodCostsDAOImpl find all method implementation

--- src/main/java/persistense/PeriodCostDaoImpl.java
+++ src/main/java/persistense/PeriodCostDaoImpl.java
@@ -3,11 +3,28 @@ package persistense;
 import costs.PeriodCostsDAO;
 import costs.PeriodCostsDTO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PeriodCostDaoImpl implements PeriodCostsDAO {
     @Override
     public List<PeriodCostsDTO> findAll() {
-        return List.of();
+        String sql = "SELECT * FROM PERIOD_COSTS";
+        List<PeriodCostsDTO> list = new ArrayList<>();
+
+        try (var statement = ConnectionFactory.prepareStatement(sql)) {
+            ResultSet rs = statement.executeQuery();
+
+            while(rs.next()) {
+                list.add(new PeriodCostsDTO(
+                        rs.getInt("hours"),
+                        rs.getDouble("fee")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
 }

54ac90da2cb6053abf3a7bfe903f7e2f31519695
Elisa <eeimymiura@gmail.com>
2026-09-01 14:30:04 -0300

    feat: connection factory and method for prepared statements

--- src/main/java/persistense/ConnectionFactory.java
+++ src/main/java/persistense/ConnectionFactory.java
@@ -1,4 +1,24 @@
 package persistense;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    private static Connection connection;
+
+    public ConnectionFactory() {}
+
+    public static Connection getConnection() throws SQLException {
+        if(connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        }
+
+        return connection;
+    }
+
+    public static PreparedStatement prepareStatement(String query) throws SQLException {
+        return getConnection().prepareStatement(query);
+    }
 }
